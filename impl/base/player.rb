38d4a12f01ffeccd0042dd5657d1ead270304d0f
Park June Chul <pjc0247@naver.com>
2015-01-06 03:07:13 +0900

    Update player.rb

--- impl/base/player.rb
+++ impl/base/player.rb
@@ -1,8 +1,17 @@
 class Player
+  attr_reader :life
+  attr_reader :weapon
+  attr_accessor :spell_damage
+  attr_reader :field
+  attr_reader :deck
+  attr_reader :hand
+  
   def initialize
     @life = 30
     @weapon = nil
     @spell = nil
+    
+    @spell_damage = 0
     
     @field = Field.new
     @deck = Deck.new
